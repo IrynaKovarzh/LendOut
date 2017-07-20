df119e6e9971a89ece3087493ad7b5f517a235f3
IrynaKovarzh <iryna.kovarzh@gmail.com>
2017-07-21 00:01:48 +0200

    added the Angular2 part

---
+++ MyLibrary/Scripts/App/app.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+	selector: "OpenList",
+	template: '<h1>OpenList</h1><div>Work in progress, la la la...</div>'  ///!!
+})
+
+export class AppComponent { }
