d57bff8b6e2ff57961b471e0689e2f781900a6db
Oskar Gewalli <oskar@gewalli.se>
2017-08-26 12:03:09 +0200

    Adding LendOut asp.net core app

---
+++ LendOut/LendOut/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace LendOut.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(245)]
+        public string Titel { get; set; }
+
+        [StringLength(50)]
+        public string Author { get; set; }
+
+        //optional
+        public int AgeCategoryId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
